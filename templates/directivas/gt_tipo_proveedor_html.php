3a48f50189440b22504dc212fb75415e9628b828
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-08-17 16:20:42 -0500

    v0.4.0 Generacion de CRUD gt_tipo_proveedor

---
+++ templates/directivas/gt_tipo_proveedor_html.php
@@ -0,0 +1,11 @@
+<?php
+namespace html;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\html_controler;
+use models\gt_tipo_proveedor;
+use PDO;
+use stdClass;
+
+class gt_tipo_proveedor_html extends html_controler {
+}
