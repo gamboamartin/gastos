fa92a85a8e1ccc2a34506ea79b6bac24fce6f087
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-08-19 16:22:01 -0500

    v0.11.0 edicion de lista Datos Fiscales en gt_provedor

--- views/gt_proveedor/ver_datos_fiscales.php
+++ views/gt_proveedor/ver_datos_fiscales.php
@@ -1,6 +1,13 @@
 <?php use config\views; ?>
 <?php /** @var \gamboamartin\gastos\controllers\controlador_gt_proveedor $controlador */ ?>
 <?php /** @var stdClass $row  viene de registros del controler*/ ?>
+<?php include("contact.php");?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
 <div class="widget  widget-box box-container form-main widget-form-cart" id="form">
     <div class="container">
         <div class="row">
@@ -26,6 +33,8 @@
                                 <th scope="col">Exterior</th>
                                 <th scope="col">Interior</th>
                                 <th scope="col">Pagina Web</th>
+                                <th scope="col">Modifica</th>
+                                <th scope="col">Contactos</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -35,51 +44,24 @@
                                 <td><?php echo $controlador->row_upd->gt_proveedor_dp_calle_pertenece_id?></td>
                                 <td><?php echo $controlador->row_upd->gt_proveedor_exterior?></td>
                                 <td><?php echo $controlador->row_upd->gt_proveedor_interior?></td>
+                                <td><?php echo $controlador->row_upd->gt_proveedor_pagina_web?></td>
                                 <td>
                                     <a href="./index.php?seccion=wt_hogar&accion=detalles_ubicacion&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>" class="btn btn-info"><i class="glyphicon glyphicon-edit"></i>
                                         Modificar
                                     </a>
                                 </td>
+                                <td>
+                                    <a >
+                                        <button type="button" class="btn btn-default" style="color: cadetblue" data-toggle="modal" data-target="#dataInfo"><i class='glyphicon glyphicon-user' ></i>
+                                        Informacion</button>
+                                    </a>
+                                </td>
+
+
                             </tr>
                             </tbody>
                         </table>
-                        <table class="table">
-                            <thead class="thead-dark">
-                            <tr>
-                                <th scope="col">Nombre</th>
-                                <th scope="col">Telefono</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td><?php echo $controlador->row_upd->gt_proveedor_contacto_1?></td>
-                                <td><?php echo $controlador->row_upd->gt_proveedor_telefono_1?></td>
-                                <td>
-                                    <a href="./index.php?seccion=wt_hogar&accion=detalles_ubicacion&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>" class="btn btn-info"><i class="glyphicon glyphicon-edit"></i>
-                                        Modificar
-                                    </a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><?php echo $controlador->row_upd->gt_proveedor_contacto_2?></td>
-                                <td><?php echo $controlador->row_upd->gt_proveedor_telefono_2?></td>
-                                <td>
-                                    <a href="./index.php?seccion=wt_hogar&accion=detalles_ubicacion&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>" class="btn btn-info"><i class="glyphicon glyphicon-edit"></i>
-                                        Modificar
-                                    </a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><?php echo $controlador->row_upd->gt_proveedor_contacto_3?></td>
-                                <td><?php echo $controlador->row_upd->gt_proveedor_telefono_3?></td>
-                                <td>
-                                    <a href="./index.php?seccion=wt_hogar&accion=detalles_ubicacion&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>" class="btn btn-info"><i class="glyphicon glyphicon-edit"></i>
-                                        Modificar
-                                    </a>
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
+
                     </div>
                 </div>
             </div><!-- /.center-content -->
@@ -87,3 +69,12 @@
     </div>
 
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<script src="js/app.js"></script>
+<script>
+    $(document).ready(function(){
+        load(1);
+    });
+</script>
