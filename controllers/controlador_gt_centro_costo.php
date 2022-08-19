8dc11b5de6a9996529b28213d664207fcb664f3c
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-08-19 17:42:21 -0500

    v0.14.0 se agrega CRUD gt_centro_costo

--- controllers/controlador_gt_centro_costo.php
+++ controllers/controlador_gt_centro_costo.php
@@ -13,21 +13,21 @@ use gamboamartin\errores\errores;
 use gamboamartin\system\links_menu;
 use gamboamartin\system\system;
 use gamboamartin\template\html;
-use html\gt_tipo_centro_costo_html;
-use models\gt_tipo_centro_costo;
+use html\gt_centro_costo_html;
+use models\gt_centro_costo;
 use PDO;
 use stdClass;
 
-class controlador_gt_tipo_centro_costo extends system {
+class controlador_gt_centro_costo extends system {
 
     public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
-        $modelo = new gt_tipo_centro_costo(link: $link);
+        $modelo = new gt_centro_costo(link: $link);
         $html_base = new html();
-        $html = new gt_tipo_centro_costo_html(html: $html_base);
+        $html = new gt_centro_costo_html(html: $html_base);
         $obj_link = new links_menu($this->registro_id);
         parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
 
-        $this->titulo_lista = 'Tipo Centro Costo';
+        $this->titulo_lista = 'Centro Costo';
 
     }
 }
---
+++ templates/directivas/gt_centro_costo_html.php
@@ -0,0 +1,10 @@
+<?php
+namespace html;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\html_controler;
+use PDO;
+use stdClass;
+
+class gt_centro_costo_html extends html_controler {
+}
