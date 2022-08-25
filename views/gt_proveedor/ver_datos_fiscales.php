f1e2b92beb4dfcf03fe447f12040dc66db80408a
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-08-25 02:36:15 -0500

    v0.20.0 se agregan vistas de datos fiscales y datos de contacto

--- views/gt_proveedor/ver_datos_fiscales.php
+++ views/gt_proveedor/ver_datos_fiscales.php
@@ -9,15 +9,17 @@ use config\views;
 ?>
 
 
+
+
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <section class="ftco-section " >
 
 
-    <div class="container">
+    <div class="container center">
        <div class="row justify-content-center">
             <div class="col-md-6 text-center mb-5">
-                <h1 class="heading-section"><?php echo $controlador->row_upd->gt_proveedor_descripcion?></h1>
+                <h1 class="heading-section"><?php echo $controlador->row_upd->descripcion?></h1>
             </div>
        </div>
         <div class="row justify-content-center">
@@ -27,78 +29,62 @@ use config\views;
                         <div class="col-lg-6 d-flex align-items-stretch">
                             <div class="info-wrap w-100 p-5">
                                 <h3 class="mb-4">Datos Generales</h3>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label class="control-label" for="img_descripcion">    </label>
+                                        <label class="control-label" for="img_descripcion">    </label>
+                                    </div>
+                                </div>
                                 <div class="dbox w-100 d-flex align-items-start">
+                                    <div class="icon d-flex align-items-center justify-content-center">
+                                        <a class="fa fa-bandcamp" style="color:#00008B; font-size: 30px"  href="./index.php?seccion=gt_proveedor&accion=ver_datos_fiscales&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>" >  Datos Generales</a>
+                                    </div>
+                                    <div class="text pl-4">
+                                        <h4><span>       Descripcion:  </span><?php echo $controlador->row_upd->descripcion; ?></h4>
+                                    </div>
+                                    <div class="text pl-4">
+                                        <h4><span>       Codigo:  </span><?php echo $controlador->row_upd->codigo; ?></h4>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label class="control-label" for="img_descripcion">    </label>
 
+                                    </div>
+                                </div>
+
+                                <div class="dbox w-100 d-flex align-items-start">
                                     <div class="icon d-flex align-items-center justify-content-center">
-                                        <a class="fa fa-bandcamp" style="color:#00008B; font-size: 30px"  href="ver_datos_fiscales.php">  Datos Generales</a>
+                                        <a class="fa fa-bandcamp" style="color:#00008B; font-size: 30px"  href="./index.php?seccion=gt_proveedor&accion=ver_datos_fiscales1&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>">  Datos Fiscales</a>
+                                    </div>
+
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label class="control-label" for="img_descripcion">    </label>
 
                                     </div>
-                                    <div class="text pl-4">
-                                        <p><span>Descripcion:</span><?php echo $controlador->row_upd->gt_proveedor_descripcion; ?></p>
-                                    </div>
                                 </div>
                                 <div class="dbox w-100 d-flex align-items-start">
                                     <div class="icon d-flex align-items-center justify-content-center">
-                                        <span class="fa fa-bandcamp"></span>
+                                        <a class="fa fa-bandcamp" style="color:#00008B; font-size: 30px"  href="./index.php?seccion=gt_proveedor&accion=ver_datos_fiscales2&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>">  Datos De contacto</a>
                                     </div>
-                                    <div class="text pl-4">
-                                        <p><span>Codigo:</span><?php echo $controlador->row_upd->gt_proveedor_codigo; ?></p>
-                                    </div>
+
                                 </div>
-                                <div class="dbox w-100 d-flex align-items-start">
-                                    <div class="icon d-flex align-items-center justify-content-center">
-                                        <span class="fa fa-bandcamp"></span>
-                                    </div>
-                                    <div class="text pl-4">
-                                        <p><span>Descripcion select:</span><?php echo $controlador->row_upd->gt_proveedor_descripcion_select; ?></p>
-                                    </div>
-                                </div>
-                                <div class="dbox w-100 d-flex align-items-start">
-                                    <div class="icon d-flex align-items-center justify-content-center">
-                                        <span class="fa fa-bandcamp"></span>
-                                    </div>
-                                    <div class="text pl-4">
-                                        <p><span>Alias:</span><?php echo $controlador->row_upd->gt_proveedor_alias; ?></p>
-                                    </div>
-                                </div>
-                                <div class="dbox w-100 d-flex align-items-start">
-                                    <div class="icon d-flex align-items-center justify-content-center">
-                                        <span class="fa fa-bandcamp"></span>
-                                    </div>
-                                    <div class="text pl-4">
-                                        <p><span>Codigo bis:</span><?php echo $controlador->row_upd->gt_proveedor_codigo_bis; ?></p>
-                                    </div>
-                                </div>
-                                <div class="dbox w-100 d-flex align-items-start">
-                                    <div class="icon d-flex align-items-center justify-content-center">
-                                        <span class="fa fa-bandcamp"></span>
-                                    </div>
-                                    <div class="text pl-4">
-                                        <p><span>Pagina web:</span><?php echo $controlador->row_upd->gt_proveedor_pagina_web; ?></p>
-                                    </div>
-                                </div>
-                                <div class="dbox w-100 d-flex align-items-start">
-                                    <div class="icon d-flex align-items-center justify-content-center">
-                                        <span class="fa fa-bandcamp"></span>
-                                    </div>
-                                    <div class="text pl-4">
-                                        <p><span>Tipo proveedor:</span><?php echo $controlador->row_upd->gt_proveedor_gt_tipo_proveedor_id; ?></p>
-                                    </div>
-                                </div>
+
                             </div>
                         </div>
                             <div class="col-lg-5 bg-info " >
 
                                 <div class="contact-wrap w-100 p-md-5 p-4">
                                     <h3 class="mb-4">Modifica</h3>
-                                    <form method="post" action="./index.php?seccion=gt_proveedor&accion=ver_datos_fiscales&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>" class="form-additional" enctype="multipart/form-data">
 
                                         <div class="row">
                                             <div class="col-md-12">
                                                 <div class="form-group">
                                                     <label class="control-label" for="descripcion">Descripcion</label>
                                                     <div class="controls">
-                                                        <input class="form-control input-lg" type="text" name="descripcion" value="<?php echo $controlador->row_upd->gt_proveedor_descripcion; ?>" class="form-control" required="" id="descripcion" placeholder="Descripcion">
+                                                        <input class="form-control input-lg" type="text" name="descripcion" value="<?php echo $controlador->row_upd->descripcion; ?>" class="form-control" required="" id="descripcion" placeholder="Descripcion">
                                                      </div>
                                                 </div>
                                             </div>
@@ -106,7 +92,7 @@ use config\views;
                                                     <div class="form-group">
                                                         <label class="control-label" for="descripcion">Codigo</label>
                                                         <div class="controls">
-                                                            <input type="text" name="Codigo" value="<?php echo $controlador->row_upd->gt_proveedor_codigo; ?>" class="form-control" required="" id="codigo" placeholder="Codigo" readonly>
+                                                            <input type="text" name="Codigo" value="<?php echo $controlador->row_upd->codigo; ?>" class="form-control" required="" id="codigo" placeholder="Codigo" readonly>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -114,7 +100,7 @@ use config\views;
                                                 <div class="form-group">
                                                     <label class="control-label" for="descripcion_select">Descripcion Select</label>
                                                     <div class="controls">
-                                                        <input class="form-control input-lg" type="text" name="descripcion_select" value="<?php echo $controlador->row_upd->gt_proveedor_descripcion_select; ?>" class="form-control" required="" id="descripcion_select" placeholder="Descripcion Select">
+                                                        <input class="form-control input-lg" type="text" name="descripcion_select" value="<?php echo $controlador->row_upd->descripcion_select; ?>" class="form-control" required="" id="descripcion_select" placeholder="Descripcion Select">
                                                     </div>
                                                 </div>
                                             </div>
@@ -122,7 +108,7 @@ use config\views;
                                                 <div class="form-group">
                                                     <label class="control-label" for="alias">Alias</label>
                                                     <div class="controls">
-                                                        <input class="form-control input-lg" type="text" name="alias" value="<?php echo $controlador->row_upd->gt_proveedor_alias; ?>" class="form-control" required="" id="alias" placeholder="Alias">
+                                                        <input class="form-control input-lg" type="text" name="alias" value="<?php echo $controlador->row_upd->alias; ?>" class="form-control" required="" id="alias" placeholder="Alias">
                                                     </div>
                                                 </div>
                                             </div>
@@ -130,7 +116,7 @@ use config\views;
                                                 <div class="form-group">
                                                     <label class="control-label" for="url">Codigo bis</label>
                                                     <div class="controls">
-                                                        <input class="form-control input-lg" type="text" name="codigo_bis" value="<?php echo $controlador->row_upd->gt_proveedor_codigo_bis; ?>" class="form-control" required="" id="codigo_bis" placeholder="codigo bis">
+                                                        <input class="form-control input-lg" type="text" name="codigo_bis" value="<?php echo $controlador->row_upd->codigo_bis; ?>" class="form-control" required="" id="codigo_bis" placeholder="codigo bis">
                                                     </div>
                                                 </div>
                                             </div>
@@ -138,7 +124,7 @@ use config\views;
                                                 <div class="form-group">
                                                     <label class="control-label" for="img_descripcion">Pagina Web</label>
                                                     <div class="controls">
-                                                        <input class="form-control input-lg" type="text" name="pagina_web" value="<?php echo $controlador->row_upd->gt_proveedor_pagina_web; ?>" class="form-control" required="" id="pagina_web" placeholder="pagina_web">
+                                                        <input class="form-control input-lg" type="text" name="pagina_web" value="<?php echo $controlador->row_upd->pagina_web; ?>" class="form-control" required="" id="pagina_web" placeholder="pagina_web">
                                                     </div>
                                                 </div>
                                             </div>
@@ -158,15 +144,20 @@ use config\views;
                                             </div>
                                             <div class="buttons col-md-12">
                                                 <div class="col-md-6 btn-ancho">
-                                                    <?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
+
+                                                    <a href="./index.php?seccion=gt_proveedor&accion=modifica_datos_fiscales&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>" class="btn btn-info"><i class=""></i>
+                                                        Modifica
+                                                    </a>
                                                 </div>
                                                 <div class="col-md-6 btn-ancho">
-                                                    <button type="submit" class="btn btn-info btn-guarda col-md-12 " name="btn_action_next" value="ubicacion">Siguiente</button>
+                                                    <a href="./index.php?seccion=gt_proveedor&accion=ver_datos_fiscales2&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>" class="btn btn-info"><i class=""></i>
+                                                        Siguiente
+                                                    </a>
                                                 </div>
                                             </div>
                                         </div>
 
-                                    </form>
+
 
 
                                 </div>
---
+++ views/gt_proveedor/ver_datos_fiscales2.php
@@ -0,0 +1,173 @@
+<?php
+
+use config\generales;
+use config\views;
+?>
+<?php /** @var base\controller\controlador_base $controlador  viene de registros del controler/lista */
+//var_dump($controlador->inputs); exit;
+//var_dump($controlador->inputs); exit;
+?>
+
+
+
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<section class="ftco-section " >
+
+
+    <div class="container center">
+        <div class="row justify-content-center">
+            <div class="col-md-6 text-center mb-5">
+                <h1 class="heading-section"><?php echo $controlador->row_upd->descripcion?></h1>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-11">
+                <div class="wrapper">
+                    <div class="row no-gutters justify-content-between">
+                        <div class="col-lg-6 d-flex align-items-stretch">
+                            <div class="info-wrap w-100 p-5">
+                                <h3 class="mb-4">Datos Generales</h3>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label class="control-label" for="img_descripcion">    </label>
+                                        <label class="control-label" for="img_descripcion">    </label>
+                                    </div>
+                                </div>
+                                <div class="dbox w-100 d-flex align-items-start">
+                                    <div class="icon d-flex align-items-center justify-content-center">
+                                        <a class="fa fa-bandcamp" style="color:#00008B; font-size: 30px"  href="./index.php?seccion=gt_proveedor&accion=ver_datos_fiscales&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>" >  Datos Generales</a>
+                                    </div>
+                                    <div class="text pl-4">
+                                        <h4><span>       Descripcion:  </span><?php echo $controlador->row_upd->descripcion; ?></h4>
+                                    </div>
+                                    <div class="text pl-4">
+                                        <h4><span>       Codigo:  </span><?php echo $controlador->row_upd->codigo; ?></h4>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label class="control-label" for="img_descripcion">    </label>
+
+                                    </div>
+                                </div>
+
+                                <div class="dbox w-100 d-flex align-items-start">
+                                    <div class="icon d-flex align-items-center justify-content-center">
+                                        <a class="fa fa-bandcamp" style="color:#00008B; font-size: 30px"  href="./index.php?seccion=gt_proveedor&accion=ver_datos_fiscales1&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>">  Datos Fiscales</a>
+                                    </div>
+
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label class="control-label" for="img_descripcion">    </label>
+
+                                    </div>
+                                </div>
+                                <div class="dbox w-100 d-flex align-items-start">
+                                    <div class="icon d-flex align-items-center justify-content-center">
+                                        <a class="fa fa-bandcamp" style="color:#00008B; font-size: 30px"  href="./index.php?seccion=gt_proveedor&accion=ver_datos_fiscales2&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>">  Datos De contacto</a>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-lg-5 bg-info " >
+
+                            <div class="contact-wrap w-100 p-md-5 p-4">
+                                <h3 class="mb-4">Modifica</h3>
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="control-label" for="descripcion">Descripcion</label>
+                                            <div class="controls">
+                                                <input class="form-control input-lg" type="text" name="descripcion" value="<?php echo $controlador->row_upd->descripcion; ?>" class="form-control" required="" id="descripcion" placeholder="Descripcion">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="control-label" for="descripcion">Codigo</label>
+                                            <div class="controls">
+                                                <input type="text" name="Codigo" value="<?php echo $controlador->row_upd->codigo; ?>" class="form-control" required="" id="codigo" placeholder="Codigo" readonly>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="control-label" for="descripcion_select">Descripcion Select</label>
+                                            <div class="controls">
+                                                <input class="form-control input-lg" type="text" name="descripcion_select" value="<?php echo $controlador->row_upd->descripcion_select; ?>" class="form-control" required="" id="descripcion_select" placeholder="Descripcion Select">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="control-label" for="alias">Alias</label>
+                                            <div class="controls">
+                                                <input class="form-control input-lg" type="text" name="alias" value="<?php echo $controlador->row_upd->alias; ?>" class="form-control" required="" id="alias" placeholder="Alias">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="control-label" for="url">Codigo bis</label>
+                                            <div class="controls">
+                                                <input class="form-control input-lg" type="text" name="codigo_bis" value="<?php echo $controlador->row_upd->codigo_bis; ?>" class="form-control" required="" id="codigo_bis" placeholder="codigo bis">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="control-label" for="img_descripcion">Pagina Web</label>
+                                            <div class="controls">
+                                                <input class="form-control input-lg" type="text" name="pagina_web" value="<?php echo $controlador->row_upd->pagina_web; ?>" class="form-control" required="" id="pagina_web" placeholder="pagina_web">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="control-label" for="gt_tipo_proveedor">Tipo proveedor</label>
+                                            <div class="controls" >
+                                                <?php echo $controlador->inputs->select->gt_tipo_proveedor_id; ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="control-label" for="img_descripcion">    </label>
+
+                                        </div>
+                                    </div>
+                                    <div class="buttons col-md-12">
+                                        <div class="col-md-6 btn-ancho">
+
+                                            <a href="./index.php?seccion=gt_proveedor&accion=modifica_datos_fiscales&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>" class="btn btn-info"><i class=""></i>
+                                                Modifica
+                                            </a>
+                                        </div>
+                                        <div class="col-md-6 btn-ancho">
+                                            <a href="./index.php?seccion=gt_proveedor&accion=ver_datos_fiscales2&registro_id=<?php echo $controlador->registro_id; ?>&session_id=<?php echo $controlador->session_id; ?>" class="btn btn-info"><i class=""></i>
+                                                Siguiente
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
