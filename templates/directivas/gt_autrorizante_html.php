f7abd419e3b42c5f708b032b3f6cb7919579cd86
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-08-29 16:49:45 -0500

    v0.32.0 se agrega CRUD gt_autorizante

---
+++ templates/directivas/gt_autrorizante_html.php
@@ -0,0 +1,13 @@
+<?php
+namespace html;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\html_controler;
+use models\gt_autorizante;
+use models\gt_tipo_solicitud;
+use PDO;
+use stdClass;
+
+class gt_autrorizante_html extends html_controler {
+
+}
