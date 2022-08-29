394fc6b5c4d39d2f6b728e2858f10071b1224a20
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-08-29 16:07:46 -0500

    v0.31.0 se agrega funcion para crear select tipo_solicitud

--- controllers/controlador_gt_solicitante.php
+++ controllers/controlador_gt_solicitante.php
@@ -25,6 +25,8 @@ class controlador_gt_solicitante extends system {
         $html_base = new html();
         $html = new gt_solicitante_html(html: $html_base);
         $obj_link = new links_menu($this->registro_id);
+        $this->rows_lista[] = 'gt_tipo_solicitud_id';
+
         parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
 
         $this->titulo_lista = 'Solicitante';
--- templates/directivas/gt_tipo_solicitud_html.php
+++ templates/directivas/gt_tipo_solicitud_html.php
@@ -3,8 +3,19 @@ namespace html;
 
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use models\gt_tipo_solicitud;
 use PDO;
 use stdClass;
 
 class gt_tipo_solicitud_html extends html_controler {
+    public function select_gt_tipo_solicitud_id(int $cols, bool $con_registros,int $id_selected, PDO $link): array|string
+    {
+        $modelo = new gt_tipo_solicitud($link);
+
+        $select = $this->select_catalogo(cols:$cols, con_registros: $con_registros,id_selected:$id_selected, modelo: $modelo);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
 }
