bf7b405800a61f97593cd54f4caa1c4b28de46f9
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-08-30 17:43:45 -0500

    v0.39.0 modificacion de selects en gt_provedor y gt_solicitante en alta

--- templates/inputs/gt_proveedor/alta.php
+++ templates/inputs/gt_proveedor/alta.php
@@ -43,9 +43,11 @@
         <input type="text" name="pagina_web" value="" class="form-control" required="" id="pagina_web" placeholder="Pagina Web">
     </div>
 </div>
-<div class="control-group col-md-6">
+<div class="control-group col-md-12">
     <label class="control-label" for="gt_tipo_proveedor">Tipo proveedor</label>
+    <div class="controls">
     <?php echo $controlador->inputs->select->gt_tipo_proveedor_id; ?>
+    </div>
 </div>
 <div class="row form-group">
     <div class="col-md-12 mb-3 mb-md-0">
@@ -59,13 +61,17 @@
         <input type="text" name="rfc" value="" class="form-control" required="" id="rfc" placeholder="RFC">
     </div>
 </div>
-<div class="control-group col-md-6">
+<div class="control-group col-md-12">
     <label class="control-label" for="cat_sat_regimen_fiscal">Regimen Fiscal</label>
+    <div class="controls">
     <?php echo $controlador->inputs->select->cat_sat_regimen_fiscal_id; ?>
+    </div>
 </div>
-<div class="control-group col-md-6">
+<div class="control-group col-md-12">
     <label class="control-label" for="dp_calle_pertenece">Calle</label>
+    <div class="controls">
     <?php echo $controlador->inputs->select->dp_calle_pertenece_id; ?>
+    </div>
 </div>
 <div class="control-group col-sm-6">
     <label class="control-label" for="exterior">Exterior</label>
@@ -121,6 +127,7 @@
         <input type="text" name="telefono_3" value="" class="form-control" required="" id="telefono_3" placeholder="Telefono 3">
     </div>
 </div>
+<br>
 <div class="control-group btn-alta">
     <div class="controls">
         <button type="submit" class="btn btn-success" name="guarda">Alta</button>
--- templates/inputs/gt_solicitante/alta.php
+++ templates/inputs/gt_solicitante/alta.php
@@ -32,14 +32,19 @@
     </div>
 </div>
 
-<div class="control-group col-md-6">
+<div class="control-group col-sm-12">
     <label class="control-label" for="gt_tipo_proveedor">Tipo proveedor</label>
+    <div class="controls">
     <?php echo $controlador->inputs->select->gt_tipo_solicitud_id; ?>
+    </div>
 </div>
 
-<div class="control-group btn-alta">
-    <div class="controls">
+<div class="control-group  col-md-12"" >
+
+    <div class="controls " >
+
         <button type="submit" class="btn btn-success" name="guarda">Alta</button>
+
         <button type="submit" class="btn btn-success" name="guarda_otro">Genero Otro</button>
     </div>
 </div>
