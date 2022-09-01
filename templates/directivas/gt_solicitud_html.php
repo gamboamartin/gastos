5c879baa648db4a6fcdd3d1448efade475114f23
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-09-01 17:48:13 -0500

    v0.48.1 se agrega paquete gt_solicitud

---
+++ templates/directivas/gt_solicitud_html.php
@@ -0,0 +1,12 @@
+<?php
+namespace html;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\html_controler;
+use models\gt_solicitud;
+use PDO;
+use stdClass;
+
+class gt_solicitud_html extends html_controler {
+
+}
