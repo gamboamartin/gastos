29fa37b5d2e1e73d47334d74ecaa4d20df5fcb99
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-09-02 17:48:10 -0500

    v0.50.1 Se genera CRUD gt_solicitantes

---
+++ templates/directivas/gt_solicitantes_html.php
@@ -0,0 +1,11 @@
+<?php
+namespace html;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\html_controler;
+use PDO;
+use stdClass;
+
+class gt_solicitantes_html extends html_controler {
+
+}
