b2a612094c1781b2ae4295fb4407065302df0b1c
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-09-02 19:32:40 -0500

    v0.51.1 se agregan selects gt_solicitante_id y gt_solicitud_id en paquete gt_solicitantes

--- templates/directivas/gt_solicitante_html.php
+++ templates/directivas/gt_solicitante_html.php
@@ -3,8 +3,19 @@ namespace html;
 
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use models\gt_solicitante;
 use PDO;
 use stdClass;
 
 class gt_solicitante_html extends html_controler {
+    public function select_gt_solicitante_id(int $cols, bool $con_registros,int $id_selected, PDO $link): array|string
+    {
+        $modelo = new gt_solicitante($link);
+
+        $select = $this->select_catalogo(cols:$cols, con_registros: $con_registros,id_selected:$id_selected, modelo: $modelo);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
 }
