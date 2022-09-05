b86c8913ff2670f48a46c9fd395950efeff95bb4
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-09-05 18:10:39 -0500

    v0.67.2 se modifico gt_centro_costo_html

--- templates/directivas/gt_centro_costo_html.php
+++ templates/directivas/gt_centro_costo_html.php
@@ -3,16 +3,25 @@ namespace html;
 
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use gamboamartin\template\directivas;
 use models\gt_centro_costo;
 use PDO;
 use stdClass;
 
 class gt_centro_costo_html extends html_controler {
-    public function select_gt_centro_costo_id(int $cols, bool $con_registros,int $id_selected, PDO $link): array|string
+    public function select_gt_centro_costo_id(int $cols, bool $con_registros, int|null $id_selected, PDO $link): array|string
     {
+        $valida = (new directivas(html:$this->html_base))->valida_cols(cols:$cols);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar cols', data: $valida);
+        }
+        if(is_null($id_selected)){
+            $id_selected = -1;
+        }
         $modelo = new gt_centro_costo($link);
 
-        $select = $this->select_catalogo(cols:$cols, con_registros: $con_registros,id_selected:$id_selected, modelo: $modelo);
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
+            modelo: $modelo,label: 'Tipo Proveedor',required: true);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
