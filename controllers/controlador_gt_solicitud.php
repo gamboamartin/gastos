9a40ddb8578a2a6342451461b6d9562def312746
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-09-05 17:43:44 -0500

    v0.64.2 se modifico controlador_gt_solicitud

--- controllers/controlador_gt_solicitud.php
+++ controllers/controlador_gt_solicitud.php
@@ -22,10 +22,10 @@ use stdClass;
 
 class controlador_gt_solicitud extends system {
 
-    public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
+    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(), stdClass $paths_conf = new stdClass()){
         $modelo = new gt_solicitud(link: $link);
-        $html_base = new html();
-        $html = new gt_solicitud_html(html: $html_base);
+
+        $html = new gt_solicitud_html(html: $html);
         $obj_link = new links_menu($this->registro_id);
         $this->rows_lista[] = 'gt_centro_costo_id';
         $this->rows_lista[] = 'gt_tipo_solicitud_id';
