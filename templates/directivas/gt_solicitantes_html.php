82da344a7d66bcc429fa91751f78de5c4afe852f
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-09-05 19:29:06 -0500

    v0.79.2 se modifico gt_solicitantes_html

--- templates/directivas/gt_solicitantes_html.php
+++ templates/directivas/gt_solicitantes_html.php
@@ -3,9 +3,29 @@ namespace html;
 
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use gamboamartin\template\directivas;
+use models\gt_solicitantes;
 use PDO;
 use stdClass;
 
 class gt_solicitantes_html extends html_controler {
+    public function select_gt_solicitantes_id(int $cols, bool $con_registros, int|null $id_selected, PDO $link): array|string
+    {
+        $valida = (new directivas(html:$this->html_base))->valida_cols(cols:$cols);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar cols', data: $valida);
+        }
+        if(is_null($id_selected)){
+            $id_selected = -1;
+        }
+        $modelo = new gt_solicitantes($link);
+
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
+            modelo: $modelo,label: 'Tipo Proveedor',required: true);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
 
 }
