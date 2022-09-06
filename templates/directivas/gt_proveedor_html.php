23aace47c4f53d980046d4ba51099659e61e168a
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-09-05 19:28:22 -0500

    v0.78.2 se modifico gt_proveedor_html

--- templates/directivas/gt_proveedor_html.php
+++ templates/directivas/gt_proveedor_html.php
@@ -3,8 +3,29 @@ namespace html;
 
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use gamboamartin\template\directivas;
+use models\gt_proveedor;
 use PDO;
 use stdClass;
 
 class gt_proveedor_html extends html_controler {
+
+    public function select_gt_proveedor_id(int $cols, bool $con_registros, int|null $id_selected, PDO $link): array|string
+    {
+        $valida = (new directivas(html:$this->html_base))->valida_cols(cols:$cols);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar cols', data: $valida);
+        }
+        if(is_null($id_selected)){
+            $id_selected = -1;
+        }
+        $modelo = new gt_proveedor($link);
+
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
+            modelo: $modelo,label: 'Tipo Proveedor',required: true);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
 }
