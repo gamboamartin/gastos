56faf15ca968d2f1b0c315dd56f8370dfca47e05
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-09-05 19:06:20 -0500

    v0.75.2 se modifico controlador_gt_tipo_proveedor

--- controllers/controlador_gt_tipo_proveedor.php
+++ controllers/controlador_gt_tipo_proveedor.php
@@ -20,10 +20,10 @@ use stdClass;
 
 class controlador_gt_tipo_proveedor extends system {
 
-    public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
+    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(), stdClass $paths_conf = new stdClass()){
         $modelo = new gt_tipo_proveedor(link: $link);
-        $html_base = new html();
-        $html = new gt_tipo_proveedor_html(html: $html_base);
+
+        $html = new gt_tipo_proveedor_html(html: $html);
         $obj_link = new links_menu($this->registro_id);
         parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
 
