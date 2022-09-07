21139a697498da4f34882319df0a61129ca0b54e
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-09-07 18:53:48 -0500

    v0.96.2 se hace requerido gt_tipo_proveedor_html

--- templates/directivas/gt_tipo_proveedor_html.php
+++ templates/directivas/gt_tipo_proveedor_html.php
@@ -9,7 +9,9 @@ use PDO;
 use stdClass;
 
 class gt_tipo_proveedor_html extends html_controler {
-    public function select_gt_tipo_proveedor_id(int $cols, bool $con_registros, int|null $id_selected, PDO $link): array|string
+    public function select_gt_tipo_proveedor_id(int $cols, bool $con_registros, int|null $id_selected, PDO $link,
+                                                bool $disabled = false, array $filtro = array(),
+                                                bool $required = false): array|string
     {
 
         $valida = (new directivas(html:$this->html_base))->valida_cols(cols:$cols);
@@ -22,8 +24,8 @@ class gt_tipo_proveedor_html extends html_controler {
 
         $modelo = new gt_tipo_proveedor($link);
 
-        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
-            modelo: $modelo,label: 'Tipo Proveedor',required: true);
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected, filtro: $filtro,
+            modelo: $modelo,label: 'Tipo Proveedor', name: 'tg_tipo_proveedor_id', disabled:$disabled, required: $required);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
