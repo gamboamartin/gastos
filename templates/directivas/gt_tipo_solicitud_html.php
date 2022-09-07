ebe849061aec5c874d182f6c17b4028d009fe0e2
jaz0193 <jazmin.rodriguez@ivitec.com.mx>
2022-09-07 18:59:14 -0500

    v0.97.2 se hace requerido gt_tipo_solicitud_html

--- templates/directivas/gt_tipo_solicitud_html.php
+++ templates/directivas/gt_tipo_solicitud_html.php
@@ -9,7 +9,9 @@ use PDO;
 use stdClass;
 
 class gt_tipo_solicitud_html extends html_controler {
-    public function select_gt_tipo_solicitud_id(int $cols, bool $con_registros, int|null $id_selected, PDO $link): array|string
+    public function select_gt_tipo_solicitud_id(int $cols, bool $con_registros, int|null $id_selected, PDO $link,
+                                                bool $disabled = false, array $filtro = array(),
+                                                bool $required = false): array|string
     {
         $valida = (new directivas(html:$this->html_base))->valida_cols(cols:$cols);
         if(errores::$error){
@@ -20,8 +22,8 @@ class gt_tipo_solicitud_html extends html_controler {
         }
         $modelo = new gt_tipo_solicitud($link);
 
-        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
-            modelo: $modelo,label: 'Tipo Solicitud',required: true);
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected, filtro: $filtro,
+            modelo: $modelo,label: 'Tipo Solicitud', name: 'tg_tipo_solicitud_id', disabled:$disabled, required: $required);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
