7a262f0d27c45189c626923efb1c335d29ae59e6
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-09-08 10:34:48 -0500

    v0.104.2 elimina descripcion_select de alta y en alta_bd se le asigna el valor de codigo + razon_social

--- templates/inputs/gt_proveedor/alta.php
+++ templates/inputs/gt_proveedor/alta.php
@@ -14,12 +14,6 @@
     </div>
 </div>
 <div class="control-group col-sm-6">
-    <label class="control-label" for="descripcion_select">Descripcion select</label>
-    <div class="controls">
-        <input type="text" name="descripcion_select" value="" class="form-control" required="" id="descripcion_select" placeholder="Descripcion select">
-    </div>
-</div>
-<div class="control-group col-sm-6">
     <label class="control-label" for="alias">Alias</label>
     <div class="controls">
         <input type="text" name="alias" value="" class="form-control" required="" id="alias" placeholder="alias">
