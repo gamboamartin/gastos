5fe7f93edf053d2e67274b11ad1c658ada967dc8
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-09-28 13:43:27 -0500

    v0.150.3 Se ajusta lista de proveedor muestra tipo proveedor

--- templates/listas/gt_proveedor/row.php
+++ templates/listas/gt_proveedor/row.php
@@ -1,11 +1,16 @@
-<?php use config\views; ?>
-<?php /** @var controllers\controlador_wt_hogar $controlador */ ?>
+<?php use config\views;
+use models\gt_tipo_proveedor; ?>
+<?php /** @var controllers\controlador_gt_proveedor $controlador */ ?>
 <?php /** @var stdClass $row  viene de registros del controler*/ ?>
+
+<?php $gt_tipo_proveedor = new gt_tipo_proveedor(link: $controlador->link);?>
+<?php $r_gt_tipo_proveedor = $gt_tipo_proveedor->registro(
+        registro_id: $row->gt_proveedor_gt_tipo_proveedor_id);?>
 
 <tr>
     <td><?php echo $row->gt_proveedor_id; ?></td>
     <td><?php echo $row->gt_proveedor_descripcion; ?></td>
-    <td><?php echo $row->gt_proveedor_gt_tipo_proveedor_id; ?></td>
+    <td><?php echo $r_gt_tipo_proveedor['gt_tipo_proveedor_descripcion']; ?></td>
     <td>
         <a href="./index.php?seccion=gt_proveedor&accion=proveedor_datos_fiscales&registro_id=<?php echo $row->gt_proveedor_id; ?>&session_id=<?php echo $controlador->session_id; ?>" class="btn btn-info"><i class=""></i>
             Datos Fiscales
