182df226664c898fab79a88ceaff2e4cdedbd0fc
Kevin <kevin.acuna@ivitec.com.mx>
2023-10-11 11:45:08 -0500

    v0.168.4 Se actualiza modulo autorizante

--- orm/gt_autorizante.php
+++ orm/gt_autorizante.php
@@ -1,20 +1,24 @@
 <?php
+
 namespace gamboamartin\gastos\models;
-use base\orm\modelo;
+
+use base\orm\_modelo_parent;
 use PDO;
 
 
-class gt_autorizante extends modelo{
-    public function __construct(PDO $link){
+class gt_autorizante extends _modelo_parent
+{
+    public function __construct(PDO $link)
+    {
         $tabla = 'gt_autorizante';
-        $columnas = array($tabla=>false);
+        $columnas = array($tabla => false, "em_empleado" => $tabla);
         $campos_obligatorios = array();
 
         $no_duplicados = array();
 
 
-        parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas,no_duplicados: $no_duplicados);
+        parent::__construct(link: $link, tabla: $tabla, campos_obligatorios: $campos_obligatorios,
+            columnas: $columnas, no_duplicados: $no_duplicados);
 
         $this->NAMESPACE = __NAMESPACE__;
     }
