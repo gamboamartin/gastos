4767c3dbc42737cdbfd52f1145f2914771fadbca
Kevin <kevin.acuna@ivitec.com.mx>
2023-10-12 12:29:45 -0500

    v0.177.4 Se actualiza solicitud

--- templates/inputs/gt_solicitud/alta.php
+++ templates/inputs/gt_solicitud/alta.php
@@ -1,7 +1,6 @@
-<?php /** @var gamboamartin\gastos\controllers\controlador_gt_tipo_solicitud $controlador  controlador en ejecucion */ ?>
+<?php /** @var  \gamboamartin\gastos\controllers\controlador_gt_solicitud $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
+<?php echo $controlador->inputs->gt_centro_costo_id; ?>
+<?php echo $controlador->inputs->gt_tipo_solicitud_id; ?>
 <?php echo $controlador->inputs->descripcion; ?>
-<?php echo $controlador->inputs->select->gt_centro_costo_id; ?>
-<?php echo $controlador->inputs->select->gt_tipo_solicitud_id; ?>
-
-<?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
+<?php include (new views())->ruta_templates.'botons/submit/alta_bd.php';?>
--- templates/inputs/gt_solicitud/modifica.php
+++ templates/inputs/gt_solicitud/modifica.php
@@ -1,15 +1,6 @@
-<?php /** @var gamboamartin\gastos\controllers\controlador_gt_tipo_solicitud $controlador  controlador en ejecucion */ ?>
+<?php /** @var gamboamartin\gastos\controllers\controlador_gt_solicitud $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
-
-<?php echo $controlador->inputs->codigo; ?>
+<?php echo $controlador->inputs->gt_centro_costo_id; ?>
+<?php echo $controlador->inputs->gt_tipo_solicitud_id; ?>
 <?php echo $controlador->inputs->descripcion; ?>
-<?php echo $controlador->inputs->select->gt_centro_costo_id; ?>
-<?php echo $controlador->inputs->select->gt_tipo_solicitud_id; ?>
-
 <?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
-
-<div class="col-row-12">
-    <?php foreach ($controlador->buttons as $button){ ?>
-        <?php echo $button; ?>
-    <?php }?>
-</div>
