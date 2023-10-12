d36b0f6fda17c969b0a21760fb9bb15f2e124d58
Kevin <kevin.acuna@ivitec.com.mx>
2023-10-12 12:43:24 -0500

    v0.178.4 Se actualizan tipos dependientes

--- orm/gt_tipo_centro_costo.php
+++ orm/gt_tipo_centro_costo.php
@@ -1,20 +1,24 @@
 <?php
+
 namespace gamboamartin\gastos\models;
+
 use base\orm\_modelo_parent_sin_codigo;
 use base\orm\modelo;
 use PDO;
 
-class gt_tipo_centro_costo extends _modelo_parent_sin_codigo {
-    public function __construct(PDO $link){
+class gt_tipo_centro_costo extends _modelo_parent_sin_codigo
+{
+    public function __construct(PDO $link)
+    {
         $tabla = 'gt_tipo_centro_costo';
-        $columnas = array($tabla=>false);
+        $columnas = array($tabla => false);
         $campos_obligatorios = array();
 
         $no_duplicados = array();
 
 
-        parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas,no_duplicados: $no_duplicados);
+        parent::__construct(link: $link, tabla: $tabla, campos_obligatorios: $campos_obligatorios,
+            columnas: $columnas, no_duplicados: $no_duplicados);
 
         $this->NAMESPACE = __NAMESPACE__;
     }
