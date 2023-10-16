cc2dbb2ddc249973f68379b3f36b98c1433d46b8
Kevin <kevin.acuna@ivitec.com.mx>
2023-10-16 13:58:48 -0500

    v0.188.4 Se integran acciones para insercion asincrona de solicitantes

--- js/gt_solicitud/modifica.js
+++ js/gt_solicitud/modifica.js
@@ -1,10 +1,12 @@
 let btn_alta_autorizante = $("#btn-alta-autorizante");
+let btn_alta_solicitante = $("#btn-alta-solicitante");
 
-let sl_em_empleado = $(".autorizantes #em_empleado_id");
+let sl_em_empleado1 = $(".autorizantes #em_empleado_id");
+let sl_em_empleado2 = $(".solicitantes #em_empleado_id");
 
 btn_alta_autorizante.click(function () {
 
-    let selected_empleado = sl_em_empleado.find('option:selected').val();
+    let selected_empleado = sl_em_empleado1.find('option:selected').val();
 
     if (selected_empleado === "") {
         alert("Seleccione un empleado");
@@ -18,7 +20,7 @@ btn_alta_autorizante.click(function () {
         data : {em_empleado_id: selected_empleado} ,
         type : 'POST',
         success : function(json) {
-            sl_em_empleado.val('').change();
+            sl_em_empleado1.val('').change();
             console.log(json);
         },
         error : function(xhr, status) {
@@ -26,5 +28,30 @@ btn_alta_autorizante.click(function () {
             console.log({xhr, status})
         }
     });
+});
 
+btn_alta_solicitante.click(function () {
+
+    let selected_empleado = sl_em_empleado2.find('option:selected').val();
+
+    if (selected_empleado === "") {
+        alert("Seleccione un empleado");
+        return;
+    }
+
+    let url = get_url("gt_solicitante","alta_bd", {});
+
+    $.ajax({
+        url : url,
+        data : {em_empleado_id: selected_empleado} ,
+        type : 'POST',
+        success : function(json) {
+            sl_em_empleado2.val('').change();
+            console.log(json);
+        },
+        error : function(xhr, status) {
+            alert('Error, ocurrio un error al ejecutar la peticion');
+            console.log({xhr, status})
+        }
+    });
 });
