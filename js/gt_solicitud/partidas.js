34aa316d32c60640414881059e887c6b24d37205
Kevin <kevin.acuna@ivitec.com.mx>
2023-10-23 13:30:50 -0500

    v0.207.4 Se integra lista de registros agregados

--- js/gt_solicitud/partidas.js
+++ js/gt_solicitud/partidas.js
@@ -39,10 +39,13 @@ btn_alta_producto.click(function () {
             sl_cat_sat_unidad.val('').change();
             txt_cantidad.val('');
 
-
             if (json.hasOwnProperty("error")) {
                 alert(json.mensaje_limpio)
+                return;
             }
+
+            $('#table-productos').DataTable().clear().destroy();
+            main_productos('gt_solicitud_producto', 'productos');
         },
         error: function (xhr, status) {
             alert('Error, ocurrio un error al ejecutar la peticion');
@@ -50,6 +53,56 @@ btn_alta_producto.click(function () {
         }
     });
 });
+
+const main_productos = (seccion, identificador) => {
+    const ruta_load = get_url(seccion, "data_ajax", {ws: 1});
+
+
+    return new DataTable(`#table-${identificador}`, {
+        dom: 'Bfrtip',
+        retrieve: true,
+        ajax: {
+            "url": ruta_load,
+            'data': function (data) {
+                data.filtros = {
+                    filtro: [{
+                        "key": "gt_solicitud.id",
+                        "valor": registro_id
+                    }]
+                }
+            },
+            "error": function (jqXHR, textStatus, errorThrown) {
+                let response = jqXHR.responseText;
+                console.log(response)
+            }
+        },
+        columns: [
+            {title: 'Id', data: `${seccion}_id`},
+            {title: 'Producto', data: `com_producto_descripcion`},
+            {title: 'Producto', data: `cat_sat_unidad_descripcion`},
+            {title: 'Cantidad', data: `${seccion}_cantidad`},
+            {title: 'Acciones', data: null},
+        ],
+        columnDefs: [
+            {
+                targets: 4,
+                render: function (data, type, row, meta) {
+                    let seccion = getParameterByName('seccion');
+                    let accion = getParameterByName('accion');
+                    let registro_id = getParameterByName('registro_id');
+
+                    let url = $(location).attr('href');
+                    url = url.replace(accion, "elimina_bd");
+                    url = url.replace(seccion, `gt_solicitud_producto`);
+                    url = url.replace(registro_id, row[`gt_solicitud_producto_id`]);
+                    return `<button  data-url="${url}" class="btn btn-danger btn-sm">Elimina</button>`;
+                }
+            }
+        ]
+    });
+}
+
+const table_1 = main_productos('gt_solicitud_producto', 'productos');
 
 
 
