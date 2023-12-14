768b022a311eafc6aff41f5b3c024be8a6fe86e4
Kevin <kevin.acuna@ivitec.com.mx>
2023-12-14 13:04:52 -0500

    v1,43 Se ajusta secciones requeridas

--- views/gt_cotizacion/modifica.php
+++ views/gt_cotizacion/modifica.php
@@ -27,16 +27,48 @@
         </div>
     </div>
 
+    <div class="container producto">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="widget  widget-box box-container form-main widget-form-cart" id="form">
+                    <div class="widget-header"
+                         style="display: flex;justify-content: space-between;align-items: center;">
+                        <h2>Producto</h2>
+                    </div>
+                    <form method="post" action="#" class="form-additional" id="frm-producto">
+                        <?php echo $controlador->inputs->com_producto_id; ?>
+                        <?php echo $controlador->inputs->cat_sat_unidad_id; ?>
+                        <?php echo $controlador->inputs->cantidad; ?>
+                        <?php echo $controlador->inputs->precio; ?>
+                        <div class="control-group btn-alta">
+                            <div class="controls">
+                                <button type="button" class="btn btn-success" value="producto" name="btn_action_next"
+                                        id="btn-alta-producto">Alta
+                                </button>
+                                <br>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
     <div class="container">
         <div class="row">
             <div class="col-md-12">
 
                 <div class="widget widget-box box-container widget-mylistings">
+                    <div class="widget-header"
+                         style="display: flex;justify-content: space-between;align-items: center;">
+                        <h2>Registro de productos</h2>
+                    </div>
 
-                    <div class="table-responsive" style="display: flex; justify-content: end;">
-                        <a role="button" title="Partidas"
-                           href="<?php echo $controlador->link_partidas; ?>"
-                           class="btn btn-success col-sm-2">Partidas</a>
+                    <div class="table-responsive">
+                        <table id="table-productos" class="table mb-0 table-striped table-sm "></table>
                     </div>
                 </div>
             </div>
