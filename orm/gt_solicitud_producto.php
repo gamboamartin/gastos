3278eff4cd64b0638faa6a5af7d39c7885158f48
Kevin <kevin.acuna@ivitec.com.mx>
2024-01-08 13:37:46 -0500

    v1.75.0 Se integra func. para valor promedios

--- orm/gt_solicitud_producto.php
+++ orm/gt_solicitud_producto.php
@@ -4,6 +4,7 @@ namespace gamboamartin\gastos\models;
 
 use gamboamartin\errores\errores;
 use PDO;
+use stdClass;
 
 class gt_solicitud_producto extends _base_transacciones
 {
@@ -31,5 +32,48 @@ class gt_solicitud_producto extends _base_transacciones
         $registros['descripcion'] = $registros['codigo'];
 
         return $registros;
+    }
+
+    public function get_precio_promedio(int $com_producto_id)
+    {
+        $productos = $this->get_productos(com_producto_id: $com_producto_id);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al obtener productos', data: $productos);
+        }
+
+        $promedio = $this->promedio_precios_productos(productos: $productos);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al calcular el promedio de precios de los productos', data: $promedio);
+        }
+
+        return $promedio;
+    }
+
+    public function get_productos(int $com_producto_id)
+    {
+        $filtro['gt_solicitud_producto.com_producto_id'] = $com_producto_id;
+
+        $datos = (new gt_solicitud_producto($this->link))->filtro_and(filtro: $filtro);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al aplicar filtro para productos', data: $datos);
+        }
+
+        return $datos;
+    }
+
+    public function promedio_precios_productos(stdClass $productos) : float
+    {
+        if ($productos->n_registros <= 0) {
+            return 0.0;
+        }
+
+        $promedio = 0.0;
+        $registros = $productos->registros;
+
+        foreach ($registros as $registro){
+            $promedio += $registro['gt_solicitud_producto_precio'];
+        }
+
+        return round($promedio / $productos->n_registros, 2);
     }
 }
