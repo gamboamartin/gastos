e126c38d8168d423fcaf8d5ea752898a7dd53513
Kevin <kevin.acuna@ivitec.com.mx>
2024-01-19 14:01:55 -0500

    v1.91.0 Se ajustan acciones en js para transacciones

--- js/gt_solicitud/modifica.js
+++ js/gt_solicitud/modifica.js
@@ -80,7 +80,7 @@ $(document).ready(function () {
         });
     }
 
-    const eliminar = (acciones) => {
+    const eliminar = (url, acciones) => {
         $.ajax({
             url: url,
             type: 'POST',
@@ -127,7 +127,7 @@ $(document).ready(function () {
 
         alta("gt_autorizantes", data, () => {
             sl_gt_autorizante.val('').change();
-            table_gt_autorizantes.clear().destroy();
+            $('#table-gt_autorizantes').DataTable().clear().destroy();
             table('gt_autorizantes', [
                 {title: "Id", data: `gt_autorizantes_id`},
                 {title: "Autorizante", data: 'em_empleado_nombre_completo'},
@@ -145,37 +145,24 @@ $(document).ready(function () {
             return;
         }
 
-        let url = get_url("gt_solicitantes", "alta_bd", {});
+        let data = {gt_solicitante_id: solicitante, gt_solicitud_id: registro_id};
 
-        $.ajax({
-            url: url,
-            data: {gt_solicitante_id: solicitante, gt_solicitud_id: registro_id},
-            type: 'POST',
-            success: function (json) {
-                sl_gt_soliciante.val('').change();
-                $('#table-gt_solicitantes').DataTable().clear().destroy();
-                table('gt_solicitantes', [
-                    {title: "Id", data: `gt_solicitantes_id`},
-                    {title: "Solicitante", data: 'em_empleado_nombre_completo'},
-                    {title: "Acciones", data: null},
-                ], filtro);
-
-                if (json.hasOwnProperty("error")) {
-                    alert(json.mensaje_limpio)
-                }
-            },
-            error: function (xhr, status) {
-                alert('Error, ocurrio un error al ejecutar la peticion');
-                console.log({xhr, status})
-            }
+        alta("gt_solicitantes", data, () => {
+            sl_gt_soliciante.val('').change();
+            $('#table-gt_solicitantes').DataTable().clear().destroy();
+            table('gt_solicitantes', [
+                {title: "Id", data: `gt_solicitantes_id`},
+                {title: "Solicitante", data: 'em_empleado_nombre_completo'},
+                {title: "Acciones", data: null},
+            ], filtro);
         });
     });
 
     table_gt_autorizantes.on('click', 'button', function (e) {
         const url = $(this).data("url");
 
-        eliminar(() => {
-            table_gt_autorizantes.clear().destroy();
+        eliminar(url, () => {
+            $('#table-gt_autorizantes').DataTable().clear().destroy();
             table('gt_autorizantes', [
                 {title: "Id", data: `gt_autorizantes_id`},
                 {title: "Autorizante", data: 'em_empleado_nombre_completo'},
@@ -187,14 +174,223 @@ $(document).ready(function () {
     table_gt_solicitantes.on('click', 'button', function (e) {
         const url = $(this).data("url");
 
-        eliminar(() => {
-            table_gt_solicitantes.clear().destroy();
+        eliminar(url, () => {
+            $('#table-gt_solicitantes').DataTable().clear().destroy();
             table('gt_solicitantes', [
                 {title: "Id", data: `gt_solicitantes_id`},
                 {title: "Solicitante", data: 'em_empleado_nombre_completo'},
                 {title: "Acciones", data: null},
             ], filtro);
         })
+    });
+
+    const main_productos = (seccion, identificador) => {
+        const ruta_load = get_url(seccion, "data_ajax", {ws: 1});
+
+
+        return new DataTable(`#${identificador}`, {
+            dom: 'Bfrtip',
+            retrieve: true,
+            ajax: {
+                "url": ruta_load,
+                'data': function (data) {
+                    data.filtros = {
+                        filtro: [{
+                            "key": "gt_solicitud_requisicion.gt_solicitud_id",
+                            "valor": registro_id
+                        }],
+                        extra_join: [
+                            {
+                                "entidad": "gt_solicitud_requisicion",
+                                "key": "gt_solicitud_id",
+                                "enlace": "gt_requisicion",
+                                "key_enlace": "id",
+                                "renombre": "gt_solicitud_requisicion"
+                            },
+                        ]
+                    }
+                },
+                "error": function (jqXHR, textStatus, errorThrown) {
+                    let response = jqXHR.responseText;
+                    console.log(response)
+                }
+            },
+            columns: [
+                {title: 'Id', data: `${seccion}_id`},
+                {title: 'Tipo', data: `gt_tipo_requisicion_descripcion`},
+                {title: 'Producto', data: `com_producto_descripcion`},
+                {title: 'Unidad', data: `cat_sat_unidad_descripcion`},
+                {title: 'Cantidad', data: `${seccion}_cantidad`},
+                {title: 'Precio', data: `${seccion}_precio`},
+                {title: 'Total', data: null},
+                {title: 'Acciones', data: null},
+            ],
+            columnDefs: [
+                {
+                    targets: 6,
+                    render: function (data, type, row, meta) {
+                        return Number(row[`${seccion}_cantidad`] * row[`${seccion}_precio`]).toFixed(2);
+                    }
+                },
+                {
+                    targets: 7,
+                    render: function (data, type, row, meta) {
+                        let seccion = getParameterByName('seccion');
+                        let accion = getParameterByName('accion');
+                        let registro_id = getParameterByName('registro_id');
+
+                        let url = $(location).attr('href');
+                        url = url.replace(accion, "elimina_bd");
+                        url = url.replace(seccion, `gt_requisicion_producto`);
+                        url = url.replace(registro_id, row[`gt_requisicion_producto_id`]);
+                        return `<button  data-url="${url}" class="btn btn-danger btn-sm">Elimina</button>`;
+                    }
+                }
+            ]
+        });
+    }
+
+    btn_alta_producto.click(function () {
+
+        let producto = sl_com_producto.find('option:selected').val();
+        let unidad = sl_cat_sat_unidad.find('option:selected').val();
+        let cantidad = txt_cantidad.val();
+        let precio = txt_precio.val();
+
+        if (producto === "") {
+            alert("Seleccione un producto");
+            return;
+        }
+
+        if (unidad === "") {
+            alert("Seleccione una unidad");
+            return;
+        }
+
+        if (cantidad === "") {
+            alert("Ingrese una cantidad");
+            return;
+        }
+
+        if (precio === "") {
+            alert("Ingrese un precio");
+            return;
+        }
+
+        let url = get_url("gt_solicitud_producto", "alta_bd", {});
+
+        $.ajax({
+            url: url,
+            data: {
+                com_producto_id: producto,
+                cat_sat_unidad_id: unidad,
+                cantidad: cantidad,
+                precio: precio,
+                gt_solicitud_id: registro_id
+            },
+            type: 'POST',
+            success: function (json) {
+                sl_com_producto.val('').change();
+                sl_cat_sat_unidad.val('').change();
+                txt_cantidad.val('');
+                txt_precio.val('');
+
+                if (json.hasOwnProperty("error")) {
+                    alert(json.mensaje_limpio)
+                    return;
+                }
+
+                $('#table-productos').DataTable().clear().destroy();
+                main_productos('gt_solicitud_producto', 'productos');
+            },
+            error: function (xhr, status) {
+                alert('Error, ocurrio un error al ejecutar la peticion');
+                console.log({xhr, status})
+            }
+        });
+    });
+
+    const table_3 = main_productos('gt_requisicion_producto', 'gt_requisicion_producto');
+
+    table_3.on('click', 'button', function (e) {
+        const url = $(this).data("url");
+
+        $.ajax({
+            url: url,
+            type: 'POST',
+            success: function (json) {
+                if (json.includes('error')) {
+                    alert("Error al eliminar el regstro")
+                    return;
+                }
+
+                $('#table-productos').DataTable().clear().destroy();
+                main_productos('gt_solicitud_producto', 'productos');
+            },
+            error: function (xhr, status) {
+                alert('Error, ocurrio un error al ejecutar la peticion');
+                console.log({xhr, status})
+            }
+        });
+    });
+
+    let getData = async (url, acciones) => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => acciones(data))
+            .catch(err => {
+                alert('Error al ejecutar');
+                console.error("ERROR: ", err.message)
+            });
+    }
+
+    sl_com_producto.change(function () {
+        let selected = $(this).find('option:selected');
+
+        let url = get_url("gt_solicitud_producto", "get_precio_promedio", {com_producto_id: selected.val()}, 0);
+
+        getData(url, (data) => {
+            txt_precio.val('');
+
+            if (data.n_registros > 0) {
+                let total = 0.0;
+                $.each(data.registros, function (index, registro) {
+                    total += parseFloat(registro.gt_solicitud_producto_precio);
+                });
+
+                let promedio = total / data.n_registros;
+                txt_precio.val(promedio.toFixed(3));
+            }
+        });
+
+    });
+
+    let timer = null;
+
+    $('#gt_solicitud_producto').on('click', 'thead:first-child, tbody', function (event) {
+
+        if (timer) {
+            clearTimeout(timer);
+        }
+
+        timer = setTimeout(() => {
+            var selectedData = table_gt_solicitud_producto.rows('.selected').data();
+
+            productos_seleccionados = [];
+
+            selectedData.each(function (value, row, data) {
+                productos_seleccionados.push(value.com_producto_id);
+            });
+
+            $('#agregar_producto').val(productos_seleccionados);
+        }, 500);
+    });
+
+    $('#form-orden').on('submit', function(e){
+        if(productos_seleccionados.length === 0) {
+            e.preventDefault();
+            alert("Seleccione un producto");
+        }
     });
 
 
