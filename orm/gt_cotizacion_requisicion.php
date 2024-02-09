63e67111b45602cce57040cabec2a35ec4e46292
Kevin <kevin.acuna@ivitec.com.mx>
2024-02-09 14:48:12 -0500

    v1.108.0 Se anexa extra column

--- orm/gt_cotizacion_requisicion.php
+++ orm/gt_cotizacion_requisicion.php
@@ -10,11 +10,11 @@ class gt_cotizacion_requisicion extends _base_transacciones
     public function __construct(PDO $link)
     {
         $tabla = 'gt_cotizacion_requisicion';
-        $columnas = array($tabla => false, "gt_cotizacion" => $tabla, "gt_requisicion" => $tabla);
+        $columnas = array($tabla => false, "gt_cotizacion" => $tabla, "gt_requisicion" => $tabla,
+            "gt_tipo_cotizacion" => "gt_cotizacion", "gt_proveedor" => "gt_cotizacion");
         $campos_obligatorios = array();
 
         $no_duplicados = array();
-
 
         parent::__construct(link: $link, tabla: $tabla, campos_obligatorios: $campos_obligatorios,
             columnas: $columnas, no_duplicados: $no_duplicados);
