fb1e06c4f074ce19263f48bf7aa8d9944412d45a
Kevin <kevin.acuna@ivitec.com.mx>
2024-02-19 15:00:55 -0500

    v1.130.0 Se integran acciones para relacion de cotizaciones y requisiciones

--- orm/gt_cotizacion.php
+++ orm/gt_cotizacion.php
@@ -27,4 +27,72 @@ class gt_cotizacion extends _modelo_parent_sin_codigo
         $this->NAMESPACE = __NAMESPACE__;
     }
 
+    public function alta_bd(array $keys_integra_ds = array('codigo', 'descripcion')): array|stdClass
+    {
+        $gt_requisicion_id = $this->registro['gt_requisicion_id'];
+
+        $acciones = $this->acciones_requisicion();
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al ejecutar acciones para requisicion', data: $acciones);
+        }
+
+        $r_alta_bd = parent::alta_bd($keys_integra_ds);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al insertar cotizacion', data: $r_alta_bd);
+        }
+
+        $relacon = $this->alta_relacion_requisicion_cotizacion(gt_requisicion_id: $gt_requisicion_id,
+            gt_cotizacion_id: $r_alta_bd->registro_id);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al insertar relacion entre requisicion y cotizacion', data: $relacon);
+        }
+
+        return $r_alta_bd;
+    }
+
+    public function alta_relacion_requisicion_cotizacion(int $gt_requisicion_id, int $gt_cotizacion_id,): array|stdClass
+    {
+        $registros = array();
+        $registros['gt_requisicion_id'] = $gt_requisicion_id;
+        $registros['gt_cotizacion_id'] = $gt_cotizacion_id;
+        $alta = (new gt_cotizacion_requisicion($this->link))->alta_registro(registro: $registros);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al ejecutar insercion de datos para requisicion y cotizacion',
+                data: $alta);
+        }
+
+        return $alta;
+    }
+
+    public function acciones_requisicion(): array
+    {
+        $resultado = $this->verificar_estado_requisicion(registros: $this->registro);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al verificar etapa de la requisicion', data: $resultado);
+        }
+
+        $this->registro = $this->limpia_campos_extras(registro: $this->registro, campos_limpiar: array("gt_requisicion_id"));
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al limpiar campos', data: $this->registro);
+        }
+
+        return $this->registro;
+    }
+
+    public function verificar_estado_requisicion(array $registros): array|stdClass
+    {
+        $filtro['gt_requisicion_etapa.gt_requisicion_id'] = $registros['gt_requisicion_id'];
+        $filtro['gt_requisicion.etapa'] = "AUTORIZADO";
+        $etapa = (new gt_requisicion_etapa($this->link))->filtro_and(filtro: $filtro);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al filtrar etapa de la requisicion', data: $etapa);
+        }
+
+        if ($etapa->n_registros <= 0) {
+            return $this->error->error(mensaje: 'Error la requisicion no se encuentra AUTORIZADA', data: $etapa);
+        }
+
+        return $etapa;
+    }
+
 }
