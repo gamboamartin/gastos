95430e830954f1a5b63ad375ee4d18b612472ad1
Kevin <kevin.acuna@ivitec.com.mx>
2024-02-29 14:39:12 -0500

    v1.158.1 Se integra func. en modelo

--- orm/gt_centro_costo.php
+++ orm/gt_centro_costo.php
@@ -4,7 +4,9 @@ namespace gamboamartin\gastos\models;
 
 use base\orm\_modelo_parent;
 use base\orm\modelo;
+use gamboamartin\errores\errores;
 use PDO;
+use stdClass;
 
 class gt_centro_costo extends _modelo_parent
 {
@@ -22,5 +24,26 @@ class gt_centro_costo extends _modelo_parent
 
 
         $this->NAMESPACE = __NAMESPACE__;
+    }
+
+    public function obtener_ordenes(int $gt_centro_costo_id): array|stdClass
+    {
+        $cotizaciones = $this->obtener_cotizaciones(gt_centro_costo_id: $gt_centro_costo_id);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al obtener cotizaciones', data: $cotizaciones);
+        }
+
+        return $cotizaciones->registros;
+    }
+
+    public function obtener_cotizaciones(int $gt_centro_costo_id): array|stdClass
+    {
+        $filtro['gt_cotizacion.gt_centro_costo_id'] = $gt_centro_costo_id;
+        $cotizaciones = (new gt_cotizacion($this->link))->filtro_and(filtro: $filtro);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error filtrar cotizacion', data: $cotizaciones);
+        }
+
+        return $cotizaciones;
     }
 }
