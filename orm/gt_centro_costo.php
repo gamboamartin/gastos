62dcfafb1821ea2a5924f62a2583ffc5c32c29dc
Kevin <kevin.acuna@ivitec.com.mx>
2024-03-05 14:49:02 -0500

    v1.166.1 Se documentan funcs. de calculo aplicadas

--- orm/gt_centro_costo.php
+++ orm/gt_centro_costo.php
@@ -27,7 +27,74 @@ class gt_centro_costo extends _modelo_parent
 
     }
 
-    public function ordenes_cotizacion(int $gt_centro_costo_id): array|stdClass|float
+    /**
+     * Función para aplanar un array de datos y extraer los valores de una columna específica.
+     *
+     * @param array $datos El array de datos original con filas asociativas.
+     * @param string $columna El nombre de la columna cuyos valores se desean extraer.
+     *
+     * @return array Retorna un array que contiene todos los valores de la columna especificada.
+     */
+    function aplanar(array $datos, string $columna): array
+    {
+        return array_column(array_filter($datos, function ($fila) use ($columna) {
+            return isset($fila[$columna]);
+        }), $columna);
+    }
+
+    /**
+     * Función para obtener cotizaciones filtradas por el ID de centro de costo.
+     *
+     * @param int $gt_centro_costo_id El ID del centro de costo.
+     *
+     * @return array|stdClass Retorna un array de cotizaciones o un objeto stdClass vacío.
+     * Si se produce un error durante la filtración, se devuelve un objeto de error.
+     */
+    public function obtener_cotizaciones(int $gt_centro_costo_id): array|stdClass
+    {
+        $filtro['gt_cotizacion.gt_centro_costo_id'] = $gt_centro_costo_id;
+        $cotizaciones = (new gt_cotizacion($this->link))->filtro_and(filtro: $filtro);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error filtrar cotizacion', data: $cotizaciones);
+        }
+
+        return $cotizaciones;
+    }
+
+    /**
+     * Función para obtener el ID de la orden de compra asociada a una cotización.
+     *
+     * @param int $gt_cotizacion_id El ID de la cotización.
+     *
+     * @return array|stdClass|int Retorna el ID de la orden de compra o un objeto stdClass vacío.
+     * Si no se encuentra una orden de compra, se devuelve -1.
+     * Si se produce un error durante la consulta, se devuelve un objeto de error.
+     */
+    public function obtener_orden_compra_cotizacion(int $gt_cotizacion_id): array|stdClass|int
+    {
+        $filtro = ['gt_orden_compra_cotizacion.gt_cotizacion_id' => $gt_cotizacion_id];
+        $orden = (new gt_orden_compra_cotizacion($this->link))->filtro_and(
+            columnas: ['gt_orden_compra_id'],
+            filtro: $filtro
+        );
+        if (errores::$error) {
+            return $this->error->error('Error filtrar orden compra cotizacion', $orden);
+        }
+
+        $registro = $orden->registros[0] ?? null;
+
+        return $registro ? $registro['gt_orden_compra_id'] : -1;
+    }
+
+    /**
+     * Función para obtener el total de las ordenes de compra asociadas a cotizaciones de un centro de costo.
+     *
+     * @param int $gt_centro_costo_id El ID del centro de costo para el cual se obtendrán las ordenes de compra.
+     *
+     * @return array|stdClass|float Retorna el total de las ordenes de compra, o un objeto stdClass vacío,
+     * o un objeto de error en caso de fallo durante el proceso.
+     */
+    public function total_ordenes_cotizacion(int $gt_centro_costo_id): array|stdClass|float
     {
         $cotizaciones = $this->obtener_cotizaciones(gt_centro_costo_id: $gt_centro_costo_id);
         if (errores::$error) {
@@ -60,6 +127,14 @@ class gt_centro_costo extends _modelo_parent
         return number_format(num: $total, decimals: 2);
     }
 
+    /**
+     * Función para calcular la suma de los productos asociados a una orden de compra.
+     *
+     * @param int $gt_orden_compra_id El ID de la orden de compra para la cual se calculará la suma.
+     *
+     * @return array|stdClass|float Retorna la suma de los productos, o un objeto stdClass vacío,
+     * o un objeto de error en caso de fallo durante el proceso.
+     */
     public function suma_productos_orden_compra(int $gt_orden_compra_id): array|stdClass|float
     {
         $campos = array("gt_orden_compra_producto_total");
@@ -80,6 +155,14 @@ class gt_centro_costo extends _modelo_parent
         return number_format(num: $suma, decimals: 2);
     }
 
+    /**
+     * Función para sumar los valores de una columna en un array de datos.
+     *
+     * @param array $datos El array de datos del cual se sumarán los valores.
+     * @param string $columna El nombre de la columna cuyos valores se sumarán.
+     *
+     * @return float Retorna la suma de los valores de la columna especificada.
+     */
     function sumar(array $datos, string $columna): float
     {
         $valores = $this->aplanar(datos: $datos, columna: $columna);
@@ -88,57 +171,6 @@ class gt_centro_costo extends _modelo_parent
         }
 
         return number_format(num: array_sum($valores), decimals: 2);
-    }
-
-
-    public function obtener_orden_compra_cotizacion(int $gt_cotizacion_id): array|stdClass|int
-    {
-        $filtro = ['gt_orden_compra_cotizacion.gt_cotizacion_id' => $gt_cotizacion_id];
-        $orden = (new gt_orden_compra_cotizacion($this->link))->filtro_and(
-            columnas: ['gt_orden_compra_id'],
-            filtro: $filtro
-        );
-        if (errores::$error) {
-            return $this->error->error('Error filtrar orden compra cotizacion', $orden);
-        }
-
-        $registro = $orden->registros[0] ?? null;
-
-        return $registro ? $registro['gt_orden_compra_id'] : -1;
-    }
-
-    /**
-     * Función para obtener cotizaciones filtradas por el ID de centro de costo.
-     *
-     * @param int $gt_centro_costo_id El ID del centro de costo.
-     *
-     * @return array|stdClass Retorna un array de cotizaciones o un objeto stdClass vacío.
-     * Si se produce un error durante la filtración, se devuelve un objeto de error.
-     */
-    public function obtener_cotizaciones(int $gt_centro_costo_id): array|stdClass
-    {
-        $filtro['gt_cotizacion.gt_centro_costo_id'] = $gt_centro_costo_id;
-        $cotizaciones = (new gt_cotizacion($this->link))->filtro_and(filtro: $filtro);
-        if (errores::$error) {
-            return $this->error->error(mensaje: 'Error filtrar cotizacion', data: $cotizaciones);
-        }
-
-        return $cotizaciones;
-    }
-
-    /**
-     * Función para aplanar un array de datos y extraer los valores de una columna específica.
-     *
-     * @param array $datos El array de datos original con filas asociativas.
-     * @param string $columna El nombre de la columna cuyos valores se desean extraer.
-     *
-     * @return array Retorna un array que contiene todos los valores de la columna especificada.
-     */
-    function aplanar(array $datos, string $columna): array
-    {
-        return array_column(array_filter($datos, function ($fila) use ($columna) {
-            return isset($fila[$columna]);
-        }), $columna);
     }
 
 }
