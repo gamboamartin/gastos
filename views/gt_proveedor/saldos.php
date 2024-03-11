171929583a71b9d4cdd5d408eed0df4255f34598
Kevin <kevin.acuna@ivitec.com.mx>
2024-03-11 12:16:56 -0500

    v1.76.1 Se integra view

---
+++ views/gt_proveedor/saldos.php
@@ -0,0 +1,24 @@
+<?php /** @var  \gamboamartin\gastos\controllers\controlador_gt_centro_costo $controlador controlador en ejecucion */ ?>
+<?php use config\views; ?>
+
+<main class="main section-color-primary">
+
+</main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
