f2353c3059eb013b1bcb65053be900165463c91a
Kevin <kevin.acuna@ivitec.com.mx>
2024-04-12 13:02:33 -0500

    v1.259.3 Se ajustan table proceso en view

--- js/gt_autorizante/alta.js
+++ js/gt_autorizante/alta.js
@@ -5,38 +5,15 @@ $(document).ready(function () {
     const columns_pr_proceso = [
         {
             title: "Id",
-            data: null,
-            'checkboxes':{
-
-                'selectRow':true
-            },
-        },
-        {
-            title: "Id",
             data: "pr_proceso_id"
         },
         {
             title: "Tipo",
             data: "pr_proceso_descripcion"
         }
-
     ];
-    const callback_pr_proceso = (seccion, columns) => {
-        return [
-            {
-                'targets': 0,
-                'searchable': false,
-                'orderable': false,
-                'className': 'dt-body-center',
-                'render': function (data, type, full, meta){
-                    return '<input type="checkbox" name="id[]" value="' + $('<div/>').text(data).html() + '">';
-                }
-            }
-        ]
-    }
 
-
-    const table_pr_proceso = table('pr_proceso', columns_pr_proceso, [], [], callback_pr_proceso);
+    const table_pr_proceso = table('pr_proceso', columns_pr_proceso, [], [], function () {}, true);
 
 });
 
