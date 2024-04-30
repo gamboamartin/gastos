222e3babcbefd9164c3cc1955d95fe16c7ee75f7
Kevin <kevin.acuna@ivitec.com.mx>
2024-04-30 14:36:31 -0500

    v1.280.4 Se ajusta view

--- templates/inputs/gt_autorizante/modifica.php
+++ templates/inputs/gt_autorizante/modifica.php
@@ -2,4 +2,11 @@
 <?php use config\views; ?>
 <?php echo $controlador->inputs->em_empleado_id; ?>
 <?php echo $controlador->inputs->codigo; ?>
+
+    <div class="col-md-12">
+        <table id="table-pr_proceso" class="table mb-0 table-striped table-sm "></table>
+    </div>
+
+    <input type="hidden" id="pr_procesos" name="pr_procesos" required>
+
 <?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
